5bcca7ecb8c296d7e51ffb5e3cb5bc5b76751111
Toubic <tj22fe@student.lnu.se>
2017-02-18 16:30:40 +0100

    + getArtists

--- app.js
+++ app.js
@@ -3,14 +3,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var app = express();
 
+var app = express();
+app.use(bodyParser.json());
 app.listen(process.env.PORT || 5000);
-app.use(bodyParser.urlencoded({ extended: true }));
+
+var Artist = require("./model/artists");
 
 mongoose.connect('mongodb://localhost/musicstreaming');
+var db = mongoose.connection;
 
 app.get("/", function(req, res) {
 
-    res.send("ex2")
+    res.send("For the API use: /api/artists")
+});
+
+app.get("/api/artists", function(req, res) {
+
+    Artist.getArtists(function (err, artists) {
+        if(err){
+            res.send("Could not get artists");
+        }
+        res.json(artists);
+    });
 });
---
+++ model/artists.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var mongoose = require("mongoose");
+
+var artistSchema = mongoose.Schema({
+
+    _id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    href: {
+        type: String,
+        required: true
+    }
+});
+
+var Artist = module.exports = mongoose.model('Artist', artistSchema);
+
+module.exports.getArtists = function (callback){
+    Artist.find(callback);
+};
+
