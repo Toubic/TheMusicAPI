e457c677b2ac71806f580046b5027d690288a0cd
Toubic <tj22fe@student.lnu.se>
2017-02-19 18:03:23 +0100

    separate route artists

--- app.js
+++ app.js
@@ -3,12 +3,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
+var routeArtists = require("./routes/artists");
 var app = express();
 app.use(bodyParser.json());
+app.use('/api/artists', routeArtists);
 app.listen(process.env.PORT || 5000);
-
-var Artist = require("./model/artists");
 
 mongoose.connect('mongodb://localhost/musicstreaming');
 var db = mongoose.connection;
@@ -16,63 +15,4 @@ var db = mongoose.connection;
 app.get("/", function(req, res) {
 
     res.send("For the API use: /api/artists")
-});
-
-app.get("/api/artists", function(req, res) {
-
-    Artist.getArtists(function (err, artists) {
-        if(err){
-            res.send("Could not get artists");
-        }
-        res.json(artists);
-    });
-});
-
-app.get("/api/artists/:_id", function(req, res) {
-
-    var id = req.params._id;
-
-    Artist.getArtistFromID(id, function (err, artists) {
-        if(err){
-            res.send("Could not get artist");
-        }
-        res.json(artists);
-    });
-});
-
-app.post("/api/artists", function(req, res) {
-
-    var artist = req.body;
-
-    Artist.addArtist(artist, function (err, artists) {
-        if(err){
-            res.send("Could not add artist");
-        }
-        res.json(artist);
-    });
-});
-
-app.put("/api/artists/:_id", function(req, res) {
-
-    var id = req.params._id;
-    var artist = req.body;
-
-    Artist.updateArtist(id, artist, function (err, artists) {
-        if(err){
-            res.send("Could not add artist");
-        }
-        res.json(artist);
-    });
-});
-
-app.delete("/api/artists/:_id", function(req, res) {
-
-    var id = req.params._id;
-
-    Artist.deleteArtist(id, function (err, artists) {
-        if(err){
-            res.send("Could not add artist");
-        }
-        res.send("Artist deleted");
-    });
 });
---
+++ routes/artists.js
@@ -0,0 +1,66 @@
+"use strict";
+
+var express = require("express");
+var router = express.Router();
+var Artist = require("../model/artists");
+
+router.get("/", function(req, res) {
+
+    Artist.getArtists(function (err, artists) {
+        if(err){
+            res.send("Could not get artists");
+        }
+        res.json(artists);
+    });
+});
+
+router.get("/:_id", function(req, res) {
+
+    var id = req.params._id;
+
+    Artist.getArtistFromID(id, function (err, artists) {
+        if(err){
+            res.send("Could not get artist");
+        }
+        res.json(artists);
+    });
+});
+
+router.post("/", function(req, res) {
+
+    var artist = req.body;
+
+    Artist.addArtist(artist, function (err, artists) {
+        if(err){
+            res.send("Could not add artist");
+        }
+        res.json(artist);
+    });
+});
+
+router.put("/:_id", function(req, res) {
+
+    var id = req.params._id;
+    var artist = req.body;
+
+    Artist.updateArtist(id, artist, function (err, artists) {
+        if(err){
+            res.send("Could not add artist");
+        }
+        res.json(artist);
+    });
+});
+
+router.delete("/:_id", function(req, res) {
+
+    var id = req.params._id;
+
+    Artist.deleteArtist(id, function (err, artists) {
+        if(err){
+            res.send("Could not add artist");
+        }
+        res.send("Artist deleted");
+    });
+});
+
+module.exports = router;
