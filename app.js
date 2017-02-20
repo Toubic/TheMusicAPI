22c9243b11a23a03c643c11366e18afcc633511e
Toubic <tj22fe@student.lnu.se>
2017-02-20 18:50:11 +0100

    + Basic auth and gitignore

--- app.js
+++ app.js
@@ -3,21 +3,40 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var stormpath = require("express-stormpath");
+
+var config = require("./config/config");
 var routeArtists = require("./routes/artists");
 var routeAlbums = require("./routes/albums");
 var routeSongs = require("./routes/songs");
 
 var app = express();
-app.use(bodyParser.json());
-app.use('/api/artists', routeArtists);
-app.use('/api/albums', routeAlbums);
-app.use('/api/songs', routeSongs);
-app.listen(process.env.PORT || 5000);
 
+app.use(stormpath.init(app, {
+        client: {
+            apiKey: {
+                id: config.client.apiKey.id,
+                secret: config.client.apiKey.secret
+            }
+        },
+        application: {
+            href: config.application.href
+        }
+}));
+
+
+app.use(bodyParser.json());
+app.use('/api/artists', stormpath.apiAuthenticationRequired, routeArtists);
+app.use('/api/albums', stormpath.apiAuthenticationRequired, routeAlbums);
+app.use('/api/songs', stormpath.apiAuthenticationRequired, routeSongs);
+
+app.on('stormpath.ready', function () {
+    app.listen(process.env.PORT || 5000);
+});
 mongoose.connect('mongodb://localhost/musicstreaming');
 var db = mongoose.connection;
 
-app.get("/", function(req, res) {
+app.get("/", stormpath.apiAuthenticationRequired, function(req, res) {
 
     res.send("For the API use: /api/artists or /api/albums or /api/songs")
 });
---
+++ config/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+    client: {
+        apiKey: {
+            id: '4WKMHCWVNSNZEDUA78TKJ33LV',
+            secret: 'Yy+KXxONuN17oNDv1fxQgAtT+1/bmzGufaoWPgICfMs'
+        }
+    },
+    application: {
+        href: 'https://api.stormpath.com/v1/applications/1Z8Anw671FsMD9jMOvgIxg'
+    }
+};
