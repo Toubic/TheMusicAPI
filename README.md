95f3b2558340f58d6579c21feeb97571650da8ce
Toubic <tj22fe@student.lnu.se>
2017-02-23 23:04:33 +0100

    update README and error handling app.js

--- README.md
+++ README.md
@@ -4,24 +4,29 @@
 
 1. How have you implemented the idea of HATEOAS in your API? Motivate your choices and how it support the idea of HATEOAS!
 
-Answer: I have made a "href" field for every resource in the API.
+I have made a "href" field for every resource in the API.
 
 2. If your solution should implement multiple representations of the resources. How would you do it?
 
-Maybe make one big collection instead of three, a collection that contains everything.
+Maybe make one big collection instead of three, a collection that contains artist, album and songs.
 
 3. Motivate and defend your authentication solution? Why did you choose the one you did? Pros/Cons.
 
-I wanted a simple authentication and let a third party handle user data and registration of users. 
-This because securing user data and handling authentication is a difficult process which takes a lot of time and effort.
+I wanted a simple authentication because I don't feel like it is sensitive data that my app handles. So I let a third party handle user data and registration of users (Stormpath). 
+This because securing user data and handling authentication yourself is a difficult process which takes a lot of time and effort.
 
 4. Explain how your web hook works.
 
-My web hook is a slack web hook where you can supply a incoming slack Webhook URL with the "path" variable in json.
-You send the Webhook URL as a post request to the route ../webhooks/add.
-Then you can send messages to the general chat of that slack Webhook URL through sending get request
-to ../webhooks/send/(message).
+My web hook is a slack web hook where you can supply a incoming slack Webhook URL with the "path" variable in json format.
+You send the Webhook URL as a post request to the route .../webhooks/add.
+Then you can send messages to the general chat of that slack Webhook URL through sending a get request
+to .../webhooks/send/(message).
 
 5. Since this is your first own web API there are probably things you would solve in an other way looking back at this assignment. Write your thoughts down.
 
+I would have added more auto generated data posting to the API like id, HATEOAS links and number of songs in an album. Made HATEOAS links connecting songs in an album. 
+Finally I would probably have let another developer test my API to get another point of view of my API. 
+
 6. Did you do something extra besides the fundamental requirements? Explain them.
+
+
--- app.js
+++ app.js
@@ -5,15 +5,24 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var stormpath = require("express-stormpath");
 
-var config = require("./config/config");
-var routeArtists = require("./routes/artists");
-var routeAlbums = require("./routes/albums");
-var routeSongs = require("./routes/songs");
-var routeWebhooks = require("./routes/webhooks");
+try {
 
-var app = express();
+    var config = require("./config/config");
+    var routeArtists = require("./routes/artists");
+    var routeAlbums = require("./routes/albums");
+    var routeSongs = require("./routes/songs");
+    var routeWebhooks = require("./routes/webhooks");
 
-app.use(stormpath.init(app, {
+    var app = express();
+
+    if (config.client.apiKey.id === undefined || config.client.apiKey.secret === undefined)
+        throw new Error("No apiKey info given");
+    else if (config.application.href === undefined)
+        throw new Error("No application href given");
+    else if (config.database.credentials === undefined)
+        throw new Error("No database credentials given");
+
+    app.use(stormpath.init(app, {
         client: {
             apiKey: {
                 id: config.client.apiKey.id,
@@ -23,22 +32,28 @@ app.use(stormpath.init(app, {
         application: {
             href: config.application.href
         }
-}));
+    }));
 
+    app.use(bodyParser.json());
+    app.use('/api/artists', stormpath.apiAuthenticationRequired, routeArtists);
+    app.use('/api/albums', stormpath.apiAuthenticationRequired, routeAlbums);
+    app.use('/api/songs', stormpath.apiAuthenticationRequired, routeSongs);
+    app.use('/webhooks', stormpath.apiAuthenticationRequired, routeWebhooks);
 
-app.use(bodyParser.json());
-app.use('/api/artists', stormpath.apiAuthenticationRequired, routeArtists);
-app.use('/api/albums', stormpath.apiAuthenticationRequired, routeAlbums);
-app.use('/api/songs', stormpath.apiAuthenticationRequired, routeSongs);
-app.use('/webhooks', stormpath.apiAuthenticationRequired, routeWebhooks);
+    app.on('stormpath.ready', function () {
+        app.listen(process.env.PORT || 5000);
+    });
 
-app.on('stormpath.ready', function () {
-    app.listen(process.env.PORT || 5000);
-});
-mongoose.connect(config.database.credentials);
-var db = mongoose.connection;
+    mongoose.connect(config.database.credentials);
+    var db = mongoose.connection;
 
-app.get("/", stormpath.apiAuthenticationRequired, function(req, res) {
+    app.get("/", stormpath.apiAuthenticationRequired, function (req, res) {
 
-    res.send("For the API use: /api/artists or /api/albums or /api/songs")
-});
+        res.send("For the API use: /api/artists or /api/albums or /api/songs or /webhooks/add and /webhooks/send/(message)")
+    });
+
+}
+
+catch(error){
+        console.log(error.message);
+}
