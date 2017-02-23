6b139f9cc30f1c9fca111e4607ee109a1f94cce1
Toubic <tj22fe@student.lnu.se>
2017-02-23 16:56:50 +0100

    + heroku

---
+++ config/config.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = {
+    client: {
+        apiKey: {
+            id: '4WKMHCWVNSNZEDUA78TKJ33LV',
+            secret: 'Yy+KXxONuN17oNDv1fxQgAtT+1/bmzGufaoWPgICfMs'
+        }
+    },
+    application: {
+        href: 'https://api.stormpath.com/v1/applications/1Z8Anw671FsMD9jMOvgIxg'
+    },
+    database: {
+        credentials: 'mongodb://admin:_musicapi_@ds157809.mlab.com:57809/musicapi'
+    }
+
+};
